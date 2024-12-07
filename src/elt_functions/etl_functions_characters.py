6aeaf390408f70424329e4b0827848faec6f1f40
samuel347 <samuel347@gmail.com>
2024-12-07 15:49:22 -0300

    add: functions for etl tables

--- src/elt_functions/etl_functions_characters.py
+++ src/elt_functions/etl_functions_characters.py
@@ -7,68 +7,155 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, "..", ".."))
 sys.path.append(parent_dir)
 
-from src.aux_functions.data_aux_functions import (read_json, rename_columns_table,
-        split_tables, update_row_dict, save_as_parquet)
+from src.aux_functions.data_aux_functions import (
+    read_json,
+    rename_columns_table,
+    split_tables,
+    update_row,
+    save_as_parquet,
+    treatment_explode_json_tables,
+)
 from src.parameters import param_setted
 
 
-def convert_save_characters_tables(path_input:str, path_output:str):
+def convert_save_characters_tables(path_input: str, path_output: str, mode: str):
     df = read_json(path_input)
-    df = rename_columns_table(df, "characters")
-    
+    df = rename_columns_table(df, mode)
+
     (
-    df_characters, df_characters_thumbnail, df_characters_comics,
-    df_characters_series, df_characters_stories, df_characters_events,
-    df_characters_urls
+        df_characters,
+        df_characters_thumbnail,
+        df_characters_comics,
+        df_characters_series,
+        df_characters_stories,
+        df_characters_events,
+        df_characters_urls,
     ) = create_col_to_characters_tables(df)
 
-    df_characters_comics = treatment_characters_comics(df_characters_comics)
+    (
+        df_characters_comics,
+        df_characters_events,
+        df_characters_stories,
+        df_characters_series,
+        df_characters_thumbnail,
+        df_characters_urls,
+    ) = treatment_characters_tables(
+        "characters_id",
+        mode,
+        df_characters_comics,
+        df_characters_events,
+        df_characters_stories,
+        df_characters_series,
+        df_characters_thumbnail,
+        df_characters_urls,
+    )
 
-    #create_result_file(df_characters_comics, df_characters)
-
-    target_path = os.path.join(path_output, "result", "characters")
+    target_path = os.path.join(path_output, "result", mode)
     if not os.path.exists(target_path):
         os.makedirs(target_path)
 
-    save_as_parquet(df_characters, target_path, "characters_table") 
-    save_as_parquet(df_characters_thumbnail, target_path, "characters_thumbnail_table") 
-    save_as_parquet(df_characters_comics, target_path, "characters_comics_table") 
-    save_as_parquet(df_characters_series, target_path, "characters_series_table") 
-    save_as_parquet(df_characters_stories, target_path, "characters_stories_table") 
-    save_as_parquet(df_characters_events, target_path, "characters_events_table") 
-    save_as_parquet(df_characters_urls, target_path, "characters_urls_table") 
+    save_as_parquet(df_characters, target_path, "characters_table")
+    save_as_parquet(df_characters_thumbnail, target_path, "characters_thumbnail_table")
+    save_as_parquet(df_characters_comics, target_path, "characters_comics_table")
+    save_as_parquet(df_characters_series, target_path, "characters_series_table")
+    save_as_parquet(df_characters_stories, target_path, "characters_stories_table")
+    save_as_parquet(df_characters_events, target_path, "characters_events_table")
+    save_as_parquet(df_characters_urls, target_path, "characters_urls_table")
+
+    return (
+        df_characters,
+        df_characters_thumbnail,
+        df_characters_comics,
+        df_characters_series,
+        df_characters_stories,
+        df_characters_events,
+        df_characters_urls,
+    )
 
 
-    return (df_characters, df_characters_thumbnail, df_characters_comics,
-    df_characters_series, df_characters_stories, df_characters_events,
-    df_characters_urls)
+def create_col_to_characters_tables(df: pd.DataFrame):
+    col_characters = [
+        "characters_id",
+        "characters_name",
+        "characters_description",
+        "characters_modified",
+        "characters_resourceURI",
+    ]
+    col_characters_thumbnail = [
+        "characters_thumbnail",
+    ]
+    col_characters_comics = [
+        "characters_comics",
+    ]
+    col_characters_series = [
+        "characters_series",
+    ]
+    col_characters_stories = [
+        "characters_stories",
+    ]
+    col_characters_events = [
+        "characters_events",
+    ]
+    col_characters_urls = [
+        "characters_urls",
+    ]
+
+    df_characters = split_tables(df, col_characters, "characters_id")
+    df_characters_thumbnail = split_tables(
+        df, col_characters_thumbnail, "characters_id"
+    )
+    df_characters_comics = split_tables(df, col_characters_comics, "characters_id")
+    df_characters_series = split_tables(df, col_characters_series, "characters_id")
+    df_characters_stories = split_tables(df, col_characters_stories, "characters_id")
+    df_characters_events = split_tables(df, col_characters_events, "characters_id")
+    df_characters_urls = split_tables(df, col_characters_urls, "characters_id")
+
+    return (
+        df_characters,
+        df_characters_thumbnail,
+        df_characters_comics,
+        df_characters_series,
+        df_characters_stories,
+        df_characters_events,
+        df_characters_urls,
+    )
 
 
-def create_col_to_characters_tables(df:pd.DataFrame):
-     col_characters = ["characters_id", "characters_name", "characters_description", "characters_modified", "characters_resourceURI",]
-     col_characters_thumbnail = ['characters_thumbnail',]
-     col_characters_comics = ['characters_comics']
-     col_characters_series = ['characters_series', ]
-     col_characters_stories = ['characters_stories',]
-     col_characters_events = ['characters_events', ]
-     col_characters_urls = ['characters_urls',]
- 
-     df_characters = split_tables(df,col_characters, "characters_id")
-     df_characters_thumbnail = split_tables(df,col_characters_thumbnail, "characters_id")
-     df_characters_comics = split_tables(df,col_characters_comics, "characters_id")
-     df_characters_series = split_tables(df,col_characters_series, "characters_id")
-     df_characters_stories = split_tables(df,col_characters_stories, "characters_id")
-     df_characters_events = split_tables(df,col_characters_events, "characters_id")
-     df_characters_urls = split_tables(df,col_characters_urls, "characters_id")
- 
-     return (df_characters, df_characters_thumbnail, df_characters_comics,
-             df_characters_series, df_characters_stories, df_characters_events,
-             df_characters_urls)
+def treatment_characters_tables(
+    id_col_name: str,
+    mode: str,
+    df_characters_comics: pd.DataFrame,
+    df_characters_events: pd.DataFrame,
+    df_characters_stories: pd.DataFrame,
+    df_characters_series: pd.DataFrame,
+    df_characters_thumbnail: pd.DataFrame,
+    df_characters_urls: pd.DataFrame,
+):
+    df_characters_comics = treatment_explode_json_tables(
+        df_characters_comics, id_col_name, "characters_comics", mode
+    )
+    df_characters_events = treatment_explode_json_tables(
+        df_characters_events, id_col_name, "characters_events", mode
+    )
+    df_characters_stories = treatment_explode_json_tables(
+        df_characters_stories, id_col_name, "characters_stories", mode
+    )
+    df_characters_series = treatment_explode_json_tables(
+        df_characters_series, id_col_name, "characters_series", mode
+    )
+    df_characters_thumbnail = treatment_explode_json_tables(
+        df_characters_thumbnail, id_col_name, "characters_thumbnail", mode
+    )
 
+    df_characters_urls = treatment_explode_json_tables(
+        df_characters_urls, id_col_name, "characters_urls", mode, "list"
+    )
 
-def treatment_characters_comics(df_characters_comics:pd.DataFrame):
-    df_characters_comics.apply(lambda x: update_row_dict(x,"characters_id", "characters_comics"),axis=1)
-    df_characters_comics = pd.json_normalize(df_characters_comics["characters_comics"])
-    df_characters_comics = rename_columns_table(df_characters_comics, "comics", "characters_id")
-    
-    return df_characters_comics
+    return (
+        df_characters_comics,
+        df_characters_events,
+        df_characters_stories,
+        df_characters_series,
+        df_characters_thumbnail,
+        df_characters_urls,
+    )
---
+++ src/elt_functions/etl_functions_comics.py
@@ -0,0 +1,268 @@
+import os
+import sys
+
+import pandas as pd
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, "..", ".."))
+sys.path.append(parent_dir)
+
+from src.aux_functions.data_aux_functions import (
+    read_json,
+    rename_columns_table,
+    split_tables,
+    update_row,
+    save_as_parquet,
+    treatment_explode_json_tables,
+)
+
+
+def convert_save_comics_tables(path_input: str, path_output: str, mode: str):
+    df = read_json(path_input)
+    df = rename_columns_table(df, mode)
+
+    (
+        df_comics,
+        df_comics_creators,
+        df_comics_characters,
+        df_comics_stories,
+        df_comics_events,
+        df_comics_images,
+        df_comics_thumbnail,
+        df_comics_dates,
+        df_comics_prices,
+        df_comics_urls,
+        df_comics_series,
+        df_comics_variants,
+        df_comics_collectedIssues,
+        df_comics_collections,
+        df_comics_textObjects,
+    ) = create_col_to_comics_tables(df)
+
+    (
+        df_comics_characters,
+        df_comics_creators,
+        df_comics_stories,
+        df_comics_events,
+        df_comics_thumbnail,
+        df_comics_series,
+        df_comics_dates,
+        df_comics_prices,
+        df_comics_urls,
+        df_comics_images,
+    ) = treatment_comics_tables(
+        "comics_id",
+        mode,
+        df_comics_characters,
+        df_comics_creators,
+        df_comics_stories,
+        df_comics_events,
+        df_comics_thumbnail,
+        df_comics_series,
+        df_comics_dates,
+        df_comics_prices,
+        df_comics_urls,
+        df_comics_images,
+    )
+
+    target_path = os.path.join(path_output, "result", mode)
+    print(f"target_path: {target_path}")
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+
+    save_as_parquet(df_comics, target_path, "comics_table")
+    save_as_parquet(df_comics_creators, target_path, "comics_creators_table")
+    save_as_parquet(df_comics_characters, target_path, "comics_characters_table")
+    save_as_parquet(df_comics_stories, target_path, "comics_stories_table")
+    save_as_parquet(df_comics_events, target_path, "comics_events_table")
+    save_as_parquet(df_comics_images, target_path, "comics_images_table")
+    save_as_parquet(df_comics_thumbnail, target_path, "comics_thumbnail_table")
+    save_as_parquet(df_comics_dates, target_path, "comics_dates_table")
+    save_as_parquet(df_comics_prices, target_path, "comics_prices_table")
+    save_as_parquet(df_comics_urls, target_path, "comics_urls_table")
+    save_as_parquet(df_comics_series, target_path, "comics_series_table")
+    save_as_parquet(df_comics_variants, target_path, "comics_variants_table")
+    save_as_parquet(
+        df_comics_collectedIssues, target_path, "comics_collectedIssues_table"
+    )
+    save_as_parquet(df_comics_collections, target_path, "comics_collections_table")
+    save_as_parquet(df_comics_textObjects, target_path, "comics_textObjects_table")
+
+    return (
+        df_comics,
+        df_comics_creators,
+        df_comics_characters,
+        df_comics_stories,
+        df_comics_events,
+        df_comics_images,
+        df_comics_thumbnail,
+        df_comics_dates,
+        df_comics_prices,
+        df_comics_urls,
+        df_comics_series,
+        df_comics_variants,
+        df_comics_collectedIssues,
+        df_comics_collections,
+        df_comics_textObjects,
+    )
+
+
+def create_col_to_comics_tables(df: pd.DataFrame):
+    col_comics = [
+        "comics_id",
+        "comics_digitalId",
+        "comics_title",
+        "comics_issueNumber",
+        "comics_variantDescription",
+        "comics_description",
+        "comics_modified",
+        "comics_isbn",
+        "comics_upc",
+        "comics_diamondCode",
+        "comics_ean",
+        "comics_issn",
+        "comics_format",
+        "comics_pageCount",
+        "comics_resourceURI",
+    ]
+    col_comics_creators = [
+        "comics_creators",
+    ]
+    col_comics_characters = [
+        "comics_characters",
+    ]
+    col_comics_stories = [
+        "comics_stories",
+    ]
+    col_comics_events = [
+        "comics_events",
+    ]
+    col_comics_images = [
+        "comics_images",
+    ]
+    col_comics_thumbnail = [
+        "comics_thumbnail",
+    ]
+    col_comics_dates = [
+        "comics_dates",
+    ]
+    col_comics_prices = [
+        "comics_prices",
+    ]
+    col_comics_urls = [
+        "comics_urls",
+    ]
+    col_comics_series = [
+        "comics_series",
+    ]
+    col_comics_variants = [
+        "comics_variants",
+    ]
+
+    col_comics_collectedIssues = [
+        "comics_collectedIssues",
+    ]
+    col_comics_collections = [
+        "comics_collections",
+    ]
+    col_comics_textObjects = [
+        "comics_textObjects",
+    ]
+
+    df_comics = split_tables(df, col_comics, "comics_id")
+    df_comics_creators = split_tables(df, col_comics_creators, "comics_id")
+    df_comics_characters = split_tables(df, col_comics_characters, "comics_id")
+    df_comics_stories = split_tables(df, col_comics_stories, "comics_id")
+    df_comics_events = split_tables(df, col_comics_events, "comics_id")
+    df_comics_images = split_tables(df, col_comics_images, "comics_id")
+    df_comics_thumbnail = split_tables(df, col_comics_thumbnail, "comics_id")
+    df_comics_dates = split_tables(df, col_comics_dates, "comics_id")
+    df_comics_prices = split_tables(df, col_comics_prices, "comics_id")
+    df_comics_urls = split_tables(df, col_comics_urls, "comics_id")
+    df_comics_series = split_tables(df, col_comics_series, "comics_id")
+
+    df_comics_variants = split_tables(df, col_comics_variants, "comics_id")
+    df_comics_collectedIssues = split_tables(
+        df, col_comics_collectedIssues, "comics_id"
+    )
+    df_comics_collections = split_tables(df, col_comics_collections, "comics_id")
+    df_comics_textObjects = split_tables(df, col_comics_textObjects, "comics_id")
+
+    return (
+        df_comics,
+        df_comics_creators,
+        df_comics_characters,
+        df_comics_stories,
+        df_comics_events,
+        df_comics_images,
+        df_comics_thumbnail,
+        df_comics_dates,
+        df_comics_prices,
+        df_comics_urls,
+        df_comics_series,
+        df_comics_variants,
+        df_comics_collectedIssues,
+        df_comics_collections,
+        df_comics_textObjects,
+    )
+
+
+def treatment_comics_tables(
+    id_col_name: str,
+    mode: str,
+    df_comics_characters: pd.DataFrame,
+    df_comics_creators: pd.DataFrame,
+    df_comics_stories: pd.DataFrame,
+    df_comics_events: pd.DataFrame,
+    df_comics_thumbnail: pd.DataFrame,
+    df_comics_series: pd.DataFrame,
+    df_comics_dates: pd.DataFrame,
+    df_comics_prices: pd.DataFrame,
+    df_comics_urls: pd.DataFrame,
+    df_comics_images: pd.DataFrame,
+):
+
+    df_comics_characters = treatment_explode_json_tables(
+        df_comics_characters, id_col_name, "comics_characters", mode
+    )
+    df_comics_creators = treatment_explode_json_tables(
+        df_comics_creators, id_col_name, "comics_creators", mode
+    )
+    df_comics_stories = treatment_explode_json_tables(
+        df_comics_stories, id_col_name, "comics_stories", mode
+    )
+    df_comics_events = treatment_explode_json_tables(
+        df_comics_events, id_col_name, "comics_events", mode
+    )
+    df_comics_thumbnail = treatment_explode_json_tables(
+        df_comics_thumbnail, id_col_name, "comics_thumbnail", mode
+    )
+    df_comics_series = treatment_explode_json_tables(
+        df_comics_series, id_col_name, "comics_series", mode
+    )
+
+    df_comics_dates = treatment_explode_json_tables(
+        df_comics_dates, id_col_name, "comics_dates", mode, "list"
+    )
+    df_comics_prices = treatment_explode_json_tables(
+        df_comics_prices, id_col_name, "comics_prices", mode, "list"
+    )
+    df_comics_urls = treatment_explode_json_tables(
+        df_comics_urls, id_col_name, "comics_urls", mode, "list"
+    )
+    df_comics_images = treatment_explode_json_tables(
+        df_comics_images, id_col_name, "comics_images", mode, "list"
+    )
+
+    return (
+        df_comics_characters,
+        df_comics_creators,
+        df_comics_stories,
+        df_comics_events,
+        df_comics_thumbnail,
+        df_comics_series,
+        df_comics_dates,
+        df_comics_prices,
+        df_comics_urls,
+        df_comics_images,
+    )
